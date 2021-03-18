c5a5b16d3cfb2df67def6fd70ea0c38a085d2068
wendymonl <47668478+wendymonl@users.noreply.github.com>
2021-03-18 16:55:02 -0500

    Add files via upload

---
+++ as4.py
@@ -0,0 +1,78 @@
+import random
+import math
+
+from gamelib import *
+
+class ZombieCharacter(ICharacter):
+    def __init__(self, obj_id, health, x, y, map_view):
+        ICharacter.__init__(self, obj_id, health, x, y, map_view)
+
+    def selectBehavior(self):
+        prob = random.random()
+
+        # If health is less than 50%, then heal with a 10% probability
+        if prob < 0.1 and self.getHealth() < self.getInitHealth() * 0.5:
+            return HealEvent(self)
+
+        # Pick a random direction to walk 1 unit (Manhattan distance)
+        x_off = random.randint(-1, 1)
+        y_off = random.randint(-1, 1)
+
+        # Check the bounds
+        map_view = self.getMapView()
+        size_x, size_y = map_view.getMapSize()
+        x, y = self.getPos()
+        if x + x_off < 0 or x + x_off >= size_x:
+            x_off = 0
+        if y + y_off < 0 or y + y_off >= size_y:
+            y_off = 0
+
+        return MoveEvent(self, x + x_off, y + y_off)
+
+class PlayerCharacter(ICharacter):
+    def __init__(self, obj_id, health, x, y, map_view):
+        ICharacter.__init__(self, obj_id, health, x, y, map_view)
+        # You may add any instance attributes you find useful to save information between frames
+
+    def selectBehavior(self):
+        #Behavior will be based on health
+        curr_health=self.getHealth()
+        inithealth=self.getInitHealth()
+        willscan=False
+        if (curr_health % 30) == 0 or curr_health>inithealth * 0.9: #scan when health is a multiple of 30 or when basically at full health like at the beginning of the game
+            willscan=True
+            
+        #heal
+        # If health is less than 50%, then heal yourself
+        if curr_health < inithealth * 0.5:
+            return HealEvent(self)  
+
+        #attack when a previous scan showed zombies
+        results = self.getScanResults()
+        lenresults=len(results)
+        if lenresults!=0 and willscan==False:
+            for i in (0,lenresults):    
+                zombie = results[i].getID()
+                return AttackEvent(self, zombie)
+        #scan or move:
+        # Pick a random direction to walk 1 unit (Manhattan distance)
+        x_off = random.randint(-1, 1)
+        y_off = random.randint(-1, 1)  
+        # Check the bounds
+        map_view = self.getMapView()
+        size_x, size_y = map_view.getMapSize()
+        x, y = self.getPos()
+        if x + x_off < 0 or x + x_off >= size_x:
+            x_off = 0
+        if y + y_off < 0 or y + y_off >= size_y:
+            y_off=0
+        #scan every time health is a multiple of 30.
+        if willscan==True :
+            return ScanEvent(self)
+        #if not scanning, move randomly
+        else:
+            return MoveEvent(self, x + x_off, y + y_off)  
+        
+        
+        pass
+    
